5a4e6e3d85148ae195f01f53b6a47fd8d38e5002
kuldeepsaini23 <kuldeepsaini02311@gmail.com>
2023-05-04 09:52:54 +0530

    Second Commit

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,7 +1,7 @@
 //importing Bcrypt libraray
 const bcrypt = require("bcrypt");
 const User = require("../Model/User");
-const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken")
 require("dotenv").config();
 
 //signup route handler
@@ -57,21 +57,23 @@ exports.login = async (req, res) => {
   try {
     //data fetch
     const { email, password } = req.body;
-    //validation on email and password
+
+    //Validation on email and password
     if (!email || !password) {
       return res.status(400).json({
         success: false,
-        message: "PLease fill all the details carefully",
+        message: "Please fill all the details carefully",
       });
     }
 
-    //check for registered user
-    let user = await User.findOne({ email });
-    //if not a registered user
+    //emain in db present or not
+    const user = await User.findOne({ email });
+
+    //if not a registered User
     if (!user) {
       return res.status(401).json({
         success: false,
-        message: "User is not registered",
+        message: "Wrong Email",
       });
     }
 
@@ -80,37 +82,38 @@ exports.login = async (req, res) => {
       id: user._id,
       role: user.role,
     };
-    //verify password & generate a JWT token
+    //verify password and generate JWT Token
     if (await bcrypt.compare(password, user.password)) {
-      //password match
+      //Password match
       let token = jwt.sign(payload, process.env.JWT_SECRET, {
         expiresIn: "2h",
       });
 
       user = user.toObject();
+      //eske Bina bhi hota hh aur ye HW hh
       user.token = token;
       user.password = undefined;
-
       const options = {
         expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
         httpOnly: true,
       };
 
-      res.cookie("babbarCookie", token, options).status(200).json({
+      res.cookie("token", token, options).status(200).json({
         success: true,
         token,
         user,
-        message: "User Logged in successfully",
+        message: "user Logged in Successfully",
       });
     } else {
-      //passwsord do not match
+      //Password donot match
       return res.status(403).json({
         success: false,
         message: "Password Incorrect",
       });
     }
-  } catch (error) {
-    console.log(error);
+  } 
+  catch (error) {
+    console.error(error);
     return res.status(500).json({
       success: false,
       message: "Login Failure",
